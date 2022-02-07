6eb650c382e4cac91f5253ed5a819a508219a973
Hjalmtyr <hh@hi.is>
2022-02-07 09:20:32 +0000

    added cube examples

---
+++ Angel/cube-elem.js
@@ -0,0 +1,142 @@
+/////////////////////////////////////////////////////////////////
+//    Sýnidæmi í Tölvugrafík
+//     Teningur skilgreindur með hnútum og hliðum.  Teiknaður
+//     með gl.drawElements
+//
+//    Hjálmtýr Hafsteinsson, febrúar 2022
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+var numVertices  = 36;
+
+var movement = false;     // Do we rotate?
+var spinX = 0;
+var spinY = 0;
+var origX;
+var origY;
+
+var matrixLoc;
+
+// the 8 vertices of the cube
+var vertices = [
+    vec3( -0.5, -0.5,  0.5 ),
+    vec3( -0.5,  0.5,  0.5 ),
+    vec3(  0.5,  0.5,  0.5 ),
+    vec3(  0.5, -0.5,  0.5 ),
+    vec3( -0.5, -0.5, -0.5 ),
+    vec3( -0.5,  0.5, -0.5 ),
+    vec3(  0.5,  0.5, -0.5 ),
+    vec3(  0.5, -0.5, -0.5 )
+];
+
+var vertexColors = [
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+    vec4( 0.0, 1.0, 1.0, 1.0 ),  // cyan
+    vec4( 1.0, 1.0, 1.0, 1.0 )   // white
+];
+
+// indices of the 12 triangles that comprise the cube
+var indices = [
+    1, 0, 3,
+    3, 2, 1,
+    2, 3, 7,
+    7, 6, 2,
+    3, 0, 4,
+    4, 7, 3,
+    6, 5, 1,
+    1, 2, 6,
+    4, 5, 6,
+    6, 7, 4,
+    5, 4, 0,
+    0, 1, 5
+];
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // array element buffer
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+    // color array attribute buffer
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    // vertex array attribute buffer
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    matrixLoc = gl.getUniformLocation( program, "rotation" );
+
+    //event listeners for mouse
+    canvas.addEventListener("mousedown", function(e){
+        movement = true;
+        origX = e.offsetX;
+        origY = e.offsetY;
+        e.preventDefault();         // Disable drag and drop
+    } );
+
+    canvas.addEventListener("mouseup", function(e){
+        movement = false;
+    } );
+
+    canvas.addEventListener("mousemove", function(e){
+        if(movement) {
+    	    spinY = ( spinY + (e.offsetX - origX) ) % 360;
+            spinX = ( spinX + (e.offsetY - origY) ) % 360;
+            origX = e.offsetX;
+            origY = e.offsetY;
+        }
+    } );
+
+    render();
+}
+
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var mv = mat4();
+    mv = mult( mv, rotateX(spinX) );
+    mv = mult( mv, rotateY(spinY) );
+
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
+
+    gl.drawElements( gl.TRIANGLES, numVertices, gl.UNSIGNED_BYTE, 0 );
+
+    requestAnimFrame( render );
+}
+
