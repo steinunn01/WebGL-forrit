d55ef6fbc77de87d5a8dae324a1774750a20b6ec
steinunn01 <steinunnm01@gmail.com>
2023-09-02 10:43:25 +0000

    "Initial commit"

--- Angel/gasket1-lit.js
+++ Angel/gasket1-lit.js
@@ -7,13 +7,13 @@
 var gl;
 var points;
 
-var NumPoints = 10000;
+var NumPoints = 100;
 var colorLoc;
 
 window.onload = function init()
 {
     var canvas = document.getElementById( "gl-canvas" );
-    
+
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
@@ -22,7 +22,7 @@ window.onload = function init()
     //
 
     // First, initialize the corners of our gasket with three points.
-    
+
     var vertices = [
         vec2( -1, -1 ),
         vec2(  0,  1 ),
@@ -31,15 +31,16 @@ window.onload = function init()
 
     // Specify a starting point p for our iterations
     // p must lie inside any set of three vertices
-    
+
     var u = add( vertices[0], vertices[1] );
     var v = add( vertices[0], vertices[2] );
-    var p = scale( 0.25, add( u, v ) );
+    var p = scale( 0.25, add(u, v));
+    var nyrupphafspunktur = scale(1 , vec2(100, 100));
 
     // And, add our initial point into our array of points
-    
-    points = [ p ];
-    
+    points = [ nyrupphafspunktur ];
+    //points = [ p ];
+
     // Compute new points
     // Each new point is located midway between
     // last point and a randomly chosen vertex
@@ -56,27 +57,27 @@ window.onload = function init()
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
+
     //  Load shaders and initialize attribute buffers
-    
+
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
-    
+
     // Load the data into the GPU
-    
+
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
     // Associate shader variables with our data buffer
-    
+
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
     // Find the location of the variable fColor in the shader program
     colorLoc = gl.getUniformLocation( program, "fColor" );
-    
+
     render();
 };
 
